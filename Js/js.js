0d6aeacebd06d13c71ec2458aa385048d0b98d6c
Vaughn <vwalker04@yahoo.com>
2019-02-24 17:50:36 -0500

    Added a note in bio about AV Labels

--- Js/js.js
+++ Js/js.js
@@ -1,8 +1,14 @@
 $(document).ready(function (){
+
   $('#face').mousedown(function () {
     $(this).fadeTo(300,.25);
   });
+
   $('#face').mouseup(function () {
     $(this).fadeTo(300,1);
+  });
+
+  $('#website-link').hover(() => {
+    $('.parenz').toggleClass('parenz-hidden');
   });
 });
